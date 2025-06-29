bcee053c33745d4f6671baa973bb4d306684462f
Brian Yang <brianyang120488@gmail.com>
2025-06-29 02:02:59 -0400

    Started project and added NextAuth

---
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,33 @@
+import type { NextAuthOptions } from 'next-auth'
+import EmailProvider from 'next-auth/providers/email' 
+import { DynamoDB, DynamoDBClientConfig } from "@aws-sdk/client-dynamodb"
+import { DynamoDBDocument } from "@aws-sdk/lib-dynamodb"
+import { DynamoDBAdapter } from "@auth/dynamodb-adapter"
+ 
+const config: DynamoDBClientConfig = {
+  credentials: {
+    accessKeyId: process.env.AUTH_DYNAMODB_ID as string,
+    secretAccessKey: process.env.AUTH_DYNAMODB_SECRET as string,
+  },
+  region: process.env.AUTH_DYNAMODB_REGION,
+}
+ 
+const client = DynamoDBDocument.from(new DynamoDB(config), {
+  marshallOptions: {
+    convertEmptyValues: true,
+    removeUndefinedValues: true,
+    convertClassInstanceToMap: true,
+  },
+})
+
+export const options: NextAuthOptions = {
+  providers: [
+    EmailProvider({
+      server: process.env.EMAIL_SERVER,
+      from: process.env.EMAIL_FROM,
+      // maxAge: 24 * 60 * 60, // How long email links are valid for (default 24h)
+    }),
+  ],
+  adapter: DynamoDBAdapter(client),
+}
+
---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,76 @@
+{/* 
+  TODO: Image is not centered properly 
+  TODO: SVG stroke widths are inconsistent
+  */}
+
+import Image from 'next/image';
+import Link from 'next/link';
+
+export const Navbar = () => {
+  const iconSize = 28;
+  const logoSize = 150;
+  const navList = [
+    { name: 'Rackets', href: '/rackets' },
+    { name: 'Shoes', href: '/shoes' },
+    { name: 'Bags', href: '/bags' },
+    { name: 'Apparel', href: '/apparel' },
+    { name: 'Shuttles', href: '/shuttles' },
+    { name: 'String', href: '/string' },
+  ];
+
+  return (
+    <nav>
+      {/* Top Section */}
+      <div className='flex px-32 py-8 justify-between items-center'>
+        <Image
+          src='/search.svg'
+          alt='Search Icon'
+          width={iconSize}
+          height={iconSize}
+          unoptimized
+        />
+        
+        <Link href='/' replace={true}>
+          <Image
+            src='/logo.png'
+            alt='Logo'
+            width={logoSize}
+            height={logoSize}
+          />
+        </Link>
+        <div className='flex gap-6'>
+          <Image
+            src='/person.svg'
+            alt='Account Icon'
+            width={iconSize}
+            height={iconSize}
+            unoptimized
+          />
+          <Image
+            src='/bag.svg'
+            alt='Cart Icon'
+            width={iconSize}
+            height={iconSize}
+            unoptimized
+          />
+        </div>
+      </div>
+
+      {/* Bottom Section */}
+      <div className='flex justify-evenly px-50'>
+        {navList.map((item) => (
+          <Link
+            key={`collections/${item.name}`}
+            href={item.href}
+            className='text-lg font-bold'
+          >
+            {item.name}
+          </Link>
+        ))}
+      </div>
+    </nav>
+
+
+
+  );
+}
