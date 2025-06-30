4e11d050224c1a746a351441f1a09f64e2dcaf79
Brian Yang <brianyang120488@gmail.com>
2025-06-30 00:11:14 -0400

    Updated ItemCard UI

--- src/app/collections/[itemType]/components/ItemCard.tsx
+++ src/app/collections/[itemType]/components/ItemCard.tsx
@@ -2,15 +2,16 @@ import Image from 'next/image';
 
 const ItemCard = ({image, name, price} : Item) => {
   return (
-    <div className='p-4 border rounded-lg shadow-md'>
-      <Image 
-        src={image}
-        alt={name}
-        width={300}
-        height={300}
-        className='object-cover'>
-      </Image>
-      <h1>{name}</h1>
+    <div className="p-4 border rounded-lg shadow-md">
+      <div className="relative w-full h-90 rounded p-4">
+        <Image
+          src={image}
+          alt={name}
+          fill={true}
+          className="object-cover"
+        />
+      </div>
+      <h1 className="mt-2">{name}</h1>
       <h1>${price}</h1>
     </div>
   );
--- src/app/collections/[itemType]/page.tsx
+++ src/app/collections/[itemType]/page.tsx
@@ -19,7 +19,7 @@ export default async function ItemsPage({ params }: { params: Promise<{ itemType
   return (
     <>
       <span>Item Type: {itemType}</span>
-      <div className='grid grid-cols-1 lg:grid-cols-3 gap-4 p-4'>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 px-32 justify-evenly items-center'>
         {items.map((item) => (
           <ItemCard
             key={item.name}
