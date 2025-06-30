5cb2cb8015be4528a76205a36c662ead92096d5f
Brian Yang <brianyang120488@gmail.com>
2025-06-30 01:04:28 -0400

    revert for nextauth test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,10 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+<<<<<<< HEAD
 import Navbar from "./components/Navbar";
+=======
+>>>>>>> parent of bcee053 (Started project and added NextAuth)
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +31,6 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Navbar />
         {children}
       </body>
     </html>
