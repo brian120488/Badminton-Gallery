2bbdb4fe50eeb737fa6335c5fd237c2f6d6ab867
Brian Yang <brianyang120488@gmail.com>
2025-07-04 17:56:26 -0400

    Implement Stripe

---
+++ src/app/checkout/page.tsx
@@ -0,0 +1,15 @@
+'use client'; // needed for localStorage
+
+import { useAppSelector } from '@/lib/hooks'
+import Checkout from './components/Checkout.tsx'
+
+export default function CheckoutPage() {
+  const cartItems = useAppSelector((state) => state.cart.items);
+
+  return (
+    <>
+      <h1 className="text-4xl font-bold">Checkout Page</h1>
+      <Checkout />
+    </>
+  )
+}
