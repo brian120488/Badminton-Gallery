7bd5b90f259bd5fa048ae2111f342a71998f790d
Brian Yang <brianyang120488@gmail.com>
2025-07-05 02:07:31 -0400

    Fix for build

--- src/app/collections/[itemType]/components/ItemList.tsx
+++ src/app/collections/[itemType]/components/ItemList.tsx
@@ -1,12 +1,11 @@
 'use client';
 
-import React, { useEffect, useState, use } from 'react';
+import React, { useEffect, useState } from 'react';
 import ItemCard from './ItemCard'; 
 import type { Item } from '@/types/types'; 
 
 export default function ItemList({ itemType }: { itemType: string }) {
   const [items, setItems] = useState<Item[]>([]);
-  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function fetchItems() {
@@ -16,12 +15,10 @@ export default function ItemList({ itemType }: { itemType: string }) {
         setItems(data.items);
       } catch (error) {
         console.error('Error fetching items:', error);
-      } finally {
-        setLoading(false);
       }
     }
     fetchItems();
-  }, []);
+  }, [itemType]);
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 px-32 justify-evenly items-center">
