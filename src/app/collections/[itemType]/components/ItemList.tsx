d659d3dd4b499cc63abee26de315e1d2eda93b81
Brian Yang <brianyang120488@gmail.com>
2025-07-05 02:24:24 -0400

    Add suspense to fetchItems

--- src/app/collections/[itemType]/components/ItemList.tsx
+++ src/app/collections/[itemType]/components/ItemList.tsx
@@ -1,24 +1,20 @@
-'use client';
-
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import ItemCard from './ItemCard'; 
 import type { Item } from '@/types/types'; 
 
-export default function ItemList({ itemType }: { itemType: string }) {
-  const [items, setItems] = useState<Item[]>([]);
 
-  useEffect(() => {
-    async function fetchItems() {
-      try {
-        const res = await fetch(`/api/collections/${itemType}`);
-        const data = await res.json();
-        setItems(data.items);
-      } catch (error) {
-        console.error('Error fetching items:', error);
-      }
-    }
-    fetchItems();
-  }, [itemType]);
+async function fetchItems(itemType: string) {
+  try {
+    const res = await fetch(`http://localhost:3000/api/collections/${itemType}`, { cache: 'no-store' });
+    const data = await res.json();
+    return data.items;
+  } catch (error) {
+    console.error('Error fetching items:', error);
+  }
+}
+
+export default async function ItemList({ itemType }: { itemType: string }) {
+  const items: Item[] = await fetchItems(itemType);
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 px-32 justify-evenly items-center">
