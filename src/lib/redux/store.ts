34927e03fb7e4bb87b57a70ed7c474d4859d79e7
Brian Yang <brianyang120488@gmail.com>
2025-07-04 23:11:41 -0400

    Fix redux storage

--- src/lib/redux/store.ts
+++ src/lib/redux/store.ts
@@ -2,8 +2,33 @@ import { configureStore, combineReducers } from '@reduxjs/toolkit'
 import cartReducer from './cart/cartSlice'
 import { useDispatch, useSelector } from "react-redux";
 import { persistReducer } from "redux-persist";
-import storage from 'redux-persist/lib/storage';
 import { persistStore } from "redux-persist";
+
+import createWebStorage from 'redux-persist/lib/storage/createWebStorage';
+import { WebStorage } from 'redux-persist/lib/types';
+
+export function createPersistStorage(): WebStorage {
+  const isServer = typeof window === 'undefined';
+
+  // Returns noop (dummy) storage.
+  if (isServer) {
+    return {
+      getItem() {
+        return Promise.resolve(null);
+      },
+      setItem() {
+        return Promise.resolve();
+      },
+      removeItem() {
+        return Promise.resolve();
+      },
+    };
+  }
+
+  return createWebStorage('local');
+}
+
+const storage = createPersistStorage();
 
 const persistConfig = {
   key: 'cart',
