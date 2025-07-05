e6eee83ffe8eb527030143d11516185794a07fef
Brian Yang <brianyang120488@gmail.com>
2025-07-04 22:49:57 -0400

    Add Clear Cart button

--- src/app/cart/components/CartList.tsx
+++ src/app/cart/components/CartList.tsx
@@ -1,11 +1,13 @@
 'use client'; // needed for localStorage
 
-import { useAppSelector } from '@/lib/redux/store'
+import { clearCart } from '@/lib/redux/cart/cartSlice';
+import { useAppDispatch, useAppSelector } from '@/lib/redux/store'
 import Link from 'next/link';
 
 const ItemList = () => {
   const cartItems = useAppSelector((state) => state.cart.items);
   const subtotal = cartItems.reduce((acc, item) => acc + item.price, 0);
+  const dispatch = useAppDispatch();
 
   return (
     <>
@@ -19,6 +21,12 @@ const ItemList = () => {
           Checkout
         </button>
       </Link>
+      <button 
+        className="mt-2 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+        onClick={() => dispatch(clearCart())}
+        >
+        Clear Cart
+      </button>
     </>
   )
 }
