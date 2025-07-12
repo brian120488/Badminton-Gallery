5c9b86b8a01e98ed5855b213a46c09680e90f7d2
Brian Yang <brianyang120488@gmail.com>
2025-07-11 21:14:55 -0400

    Set item background to default gray

--- src/app/(pages)/collections/[itemType]/components/ItemCard.tsx
+++ src/app/(pages)/collections/[itemType]/components/ItemCard.tsx
@@ -16,7 +16,7 @@ export default function ItemCard({ item }: { item: Item }) {
   return (
       <div className='p-3 border-none rounded-lg hover:shadow-2xl'>
         <Link href={`/products/${item.id}`}>
-          <div className='relative w-100% aspect-5/6 max-h-sm overflow-hidden rounded-lg shadow-xs'>
+          <div className='relative w-100% aspect-5/6 max-h-sm overflow-hidden rounded-lg bg-[#f8f8f8]'>
             <Image
               fill
               src={item.images[0]}
