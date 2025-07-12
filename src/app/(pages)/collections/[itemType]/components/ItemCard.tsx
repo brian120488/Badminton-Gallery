3cf538305c697eaf16c4f3360dd74d5bffb5851e
Brian Yang <brianyang120488@gmail.com>
2025-07-11 21:06:27 -0400

    Update ItemCard.tsx

--- src/app/(pages)/collections/[itemType]/components/ItemCard.tsx
+++ src/app/(pages)/collections/[itemType]/components/ItemCard.tsx
@@ -14,25 +14,25 @@ export default function ItemCard({ item }: { item: Item }) {
   };
   
   return (
-      <div className="p-3 border-none rounded-lg hover:shadow-2xl">
+      <div className='p-3 border-none rounded-lg hover:shadow-2xl'>
         <Link href={`/products/${item.id}`}>
-          <div className="relative w-100% aspect-5/6 max-h-sm overflow-hidden rounded-lg">
+          <div className='relative w-100% aspect-5/6 max-h-sm overflow-hidden rounded-lg shadow-xs'>
             <Image
               fill
               src={item.images[0]}
               alt={item.name}
-              className="object-cover rounded-md transition-transform duration-300 ease-in-out hover:scale-105"
+              className='object-contain rounded-md transition-transform duration-300 ease-in-out hover:scale-105'
               sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
               priority
             />
           </div>
         </Link>
         <div className='p-4'>
-          <h1 className="text-xl font-normal">{item.name}</h1>
-          <h2 className="text-base font-normal">${item.price}</h2>
+          <h1 className='text-xl font-normal'>{item.name}</h1>
+          <h2 className='text-base font-normal'>${item.price}</h2>
           
           <button 
-            className="mt-2 border border-blue-500 text-blue-500 px-4 py-2 rounded hover:bg-blue-500 hover:text-white cursor-pointer"
+            className='mt-2 border border-blue-500 text-blue-500 px-4 py-2 rounded hover:bg-blue-500 hover:text-white cursor-pointer'
             onClick={addToCartHandler}
           >
             Add to Cart
