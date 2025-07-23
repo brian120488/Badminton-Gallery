48a659adcb703aa2529b8cab1b5f82135e6f568e
Brian Yang <brianyang120488@gmail.com>
2025-07-23 10:58:08 -0400

    Add site message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,17 +4,18 @@ import FadeIn from './components/FadeIn';
 export default function Home() {
   return (
     <div className='relative mx-32 flex flex-col items-center'>
-      <h1 className='mt-16 text-4xl font-bold'>Badminton Gallery</h1>
+      <h1 className='mt-16 text-2xl font-semibold'>We are currently adding more items. Stay tuned!</h1>
+      <h1 className='mt-8 text-4xl font-bold'>Badminton Gallery</h1>
       <h1 className='text-2xl'>&quot;Bringing the Best of Badminton to Every Player&quot;</h1>
       <div className="flex flex-col gap-y-6 mt-6">
         <FadeIn>
           <LocationCard location='Flushing' address='133-33 39th Ave, Queens, NY 11354' image="flushing.jpg" />
         </FadeIn>
         <FadeIn>
-        <LocationCard location='Brooklyn' address='974 58th St, Brooklyn, NY 11219' image="bkny.jpg" />
+          <LocationCard location='Brooklyn' address='974 58th St, Brooklyn, NY 11219' image="bkny.jpg" />
         </FadeIn>
         <FadeIn>
-        <LocationCard location='Philadelphia' address='3300 Aldine St, Philadelphia, PA 19136' image="philadelphia.jpg" />
+          <LocationCard location='Philadelphia' address='3300 Aldine St, Philadelphia, PA 19136' image="philadelphia.jpg" />
         </FadeIn>
       </div>
     </div>
