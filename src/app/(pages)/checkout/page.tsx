f0f38793b5c045e4360051ef828d1df1fffd1d9c
Brian Yang <brianyang120488@gmail.com>
2025-07-31 13:14:17 -0400

    Update page.tsx

--- src/app/(pages)/checkout/page.tsx
+++ src/app/(pages)/checkout/page.tsx
@@ -2,9 +2,8 @@ import Checkout from './components/Checkout'
 
 export default function CheckoutPage() {
   return (
-    <>
-      <h1 className="text-4xl font-bold">Checkout Page</h1>
+    <div className='p-8'>
       <Checkout />
-    </>
+    </div>
   )
 }
