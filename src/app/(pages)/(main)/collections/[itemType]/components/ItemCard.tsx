de356a32524eaa4913c25222e67cd20f2b95dcf6
Brian Yang <brianyang120488@gmail.com>
2025-08-01 17:34:53 -0400

    Add 'no-image' if no item image

--- src/app/(pages)/(main)/collections/[itemType]/components/ItemCard.tsx
+++ src/app/(pages)/(main)/collections/[itemType]/components/ItemCard.tsx
@@ -3,13 +3,15 @@ import type { Item } from '@/types/types';
 import Link from 'next/link';
 
 export default function ItemCard({ item }: { item: Item }) {
+  const image = item.images.length > 0 ? item.images[0] : '/no-image.png'
+
   return (
       <div className='p-3 border-none rounded-lg hover:shadow-2xl'>
         <Link href={`/products/${item.id}`}>
           <div className='relative w-100% aspect-5/6 max-h-sm overflow-hidden rounded-lg bg-[#f8f8f8]'>
             <Image
               fill
-              src={item.images[0]}
+              src={image}
               alt={item.name}
               className='object-contain rounded-md transition-transform duration-300 ease-in-out hover:scale-105'
               sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
