c458e844debc01b6407bf4531e33b8acc82ddfa5
Brian Yang <brianyang120488@gmail.com>
2025-08-01 16:50:59 -0400

    Add notice for only selling rackets online

--- src/app/(pages)/(main)/collections/[itemType]/page.tsx
+++ src/app/(pages)/(main)/collections/[itemType]/page.tsx
@@ -1,6 +1,7 @@
 import ItemListWrapper from './components/ItemListWrapper'
 import { getProductsByType } from '@/lib/aws/dynamo'
 import { getProductImages } from '@/lib/aws/s3';
+import { Construction } from 'lucide-react';
 
 export default async function ItemsPage({ params }: { params: Promise<{ itemType: string }> }) {
   const { itemType } = await params;
@@ -16,11 +17,24 @@ export default async function ItemsPage({ params }: { params: Promise<{ itemType
   );
 
   return (
-    <div className='mx-16 pb-16'>
-      <span className="block text-4xl font-bold text-gray-800 mt-8 mb-4">
-        {itemType.charAt(0).toUpperCase() + itemType.slice(1)}
-      </span>
-      <ItemListWrapper itemType={itemType} items={itemsWithImages} />
-    </div>
+    <>
+      {itemType == 'racket' &&
+        <div className='mx-16 pb-16'>
+          <span className="block text-4xl font-bold text-gray-800 mt-8 mb-4">
+            {itemType.charAt(0).toUpperCase() + itemType.slice(1)}
+          </span>
+          <ItemListWrapper itemType={itemType} items={itemsWithImages} />
+        </div>
+      }
+
+      {itemType != 'racket' &&
+        <div className='flex items-center gap-3 p-4 mx-96 my-8 border border-amber-400 rounded-md bg-amber-50'>
+          <Construction className='text-amber-500' size={24} />
+          <p className='text-sm text-amber-900 m-0'>
+            We are currently only selling rackets online. Other items will be available for sale online later.
+          </p>
+        </div>
+      }
+    </>
   );
 }
