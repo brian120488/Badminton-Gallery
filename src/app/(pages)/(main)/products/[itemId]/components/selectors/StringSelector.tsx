28b3ef7d9e088b5eb37d7cdedf704a17a36984c7
Brian Yang <brianyang120488@gmail.com>
2025-08-04 13:37:46 -0400

    Add all strings

--- src/app/(pages)/(main)/products/[itemId]/components/selectors/StringSelector.tsx
+++ src/app/(pages)/(main)/products/[itemId]/components/selectors/StringSelector.tsx
@@ -8,8 +8,49 @@ export default function StringSelector() {
 
   const strings = [
     'No String', 
-    'Yonex BG65',
-    'Yonex BG65 Ti',
+    'BG 65 (Black)',
+    'BG 65 (Yellow)',
+    'BG 65 Ti (Red)',
+
+    'BG 66 (Black)',
+    'BG 66 (Blue)',
+    'BG 66 (Pink)',
+    'BG 66 (Orange)',
+
+    'BG 80 (Blue)',
+    'BG 80 (Pink)',
+    'BG 80 (Neon Yellow)',
+    'BG 80 Power (Orange)',
+
+    'Nanogy 95 (Blue)',
+    'Nanogy 95 (Neon Green)',
+
+    'Nanogy 98 (Blue)',
+    'Nanogy 98 (Beige)',
+    'Nanogy 98 (Yellow)',
+
+    'Exbolt 63 (Blue)',
+    'Exbolt 63 (Red)',
+    'Exbolt 63 (Black)',
+    'Exbolt 63 (Neon Green)',
+
+    'Exbolt 65 (Red)',
+    'Exbolt 65 (Green)',
+    'Exbolt 65 (Blue)',
+
+    'Exbolt 68 (Blue)',
+    'Exbolt 68 (Green)',
+    'Exbolt 68 (Neon Yellow)',
+    'Exbolt 68 (Red)',
+
+    'VBS-61 (Pink)',
+    'VBS-61 (Blue)',
+    'VBS-66 Nano (Green)',
+    'VBS-66 Nano (Turquoise)',
+
+    'Aerobite (Red/White)',
+    'Aerobite Boost (Red/Black)',
+    'Aerobite Boost (Neon Yellow/Black)',
   ];
 
   const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
