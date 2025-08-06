60c002b80f5239d3208b6622d774931b2448ed74
Brian Yang <brianyang120488@gmail.com>
2025-08-06 16:46:49 -0400

    Update search page UI

--- src/app/(pages)/(main)/collections/page.tsx
+++ src/app/(pages)/(main)/collections/page.tsx
@@ -22,7 +22,7 @@ export default async function CollectionsPage({
       {itemsWithImages.length > 0 ? (
         <div className="mx-16 pb-16">
           <span className="block text-4xl font-bold text-gray-800 mt-8 mb-4 capitalize">
-            {search}
+            Search: &quot;{search}&quot;
           </span>
           <ItemListWrapper itemType="racket" items={itemsWithImages} />
         </div>
@@ -30,7 +30,7 @@ export default async function CollectionsPage({
         <div className="flex items-center gap-3 p-4 mx-96 my-8 border border-amber-400 rounded-md bg-amber-50">
           <Construction className="text-amber-500" size={24} />
           <p className="text-sm text-amber-900 m-0">
-            No products found for the name &quot;{search}.
+            No products found for the name &quot;{search}&quot;.
           </p>
         </div>
       )}
